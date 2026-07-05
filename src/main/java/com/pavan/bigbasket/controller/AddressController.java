4491ec80d1fbf39fc46296feaf12022c15a7d193
Pavan <pavanjanapati222@gmail.com>
2026-07-05 13:22:21 +0530

    added address related apis

--- src/main/java/com/pavan/bigbasket/controller/AddressController.java
+++ src/main/java/com/pavan/bigbasket/controller/AddressController.java
@@ -1,10 +1,25 @@
 package com.pavan.bigbasket.controller;
 
 
+import com.pavan.bigbasket.domain.Address;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("addresses")
 public class AddressController {
+
+    @GetMapping
+    public ResponseEntity<List<Address>> getAll() {
+        Address address1 = Address.builder().id(UUID.randomUUID()).city("Hyd").build();
+        Address address2 = Address.builder().id(UUID.randomUUID()).city("Chennai").build();
+        return ResponseEntity.ok(List.of(address1, address2));
+    }
 }
--- src/main/java/com/pavan/bigbasket/domain/Address.java
+++ src/main/java/com/pavan/bigbasket/domain/Address.java
@@ -3,15 +3,22 @@ package com.pavan.bigbasket.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "addresses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Address {
 
     @Id
     private UUID id;
+
+    private String city;
 
 
 }
