f17a0f1ab1dc8c03ea8499fe36bb6d075933c350
Pavan <pavanjanapati222@gmail.com>
2026-07-05 16:44:51 +0530

    added create address api

--- src/main/java/com/pavan/bigbasket/controller/AddressController.java
+++ src/main/java/com/pavan/bigbasket/controller/AddressController.java
@@ -4,10 +4,9 @@ package com.pavan.bigbasket.controller;
 import com.pavan.bigbasket.domain.Address;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,5 +20,10 @@ public class AddressController {
         Address address1 = Address.builder().id(UUID.randomUUID()).city("Hyd").build();
         Address address2 = Address.builder().id(UUID.randomUUID()).city("Chennai").build();
         return ResponseEntity.ok(List.of(address1, address2));
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> createAddress(@RequestBody Address address) {
+        return new ResponseEntity(UUID.randomUUID(), HttpStatus.CREATED);
     }
 }
